6a594aa25a52f6ffe9230be473cb847a46a8914d
flvdnc <F@CEPUS>
2016-11-14 18:39:10 +0200

    c6 ex1

--- ProjectManagement/App_Start/RouteConfig.cs
+++ ProjectManagement/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace ProjectManagement
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Project", 
+                url: "Project/doanaction", 
+                defaults: new { controller = "Project", action = "doanaction", name = UrlParameter.Optional }
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }

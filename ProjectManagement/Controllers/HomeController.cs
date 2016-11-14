50e93bb5e29173a281b20df36bb48e22fe138fd7
flvdnc <F@CEPUS>
2016-11-14 18:57:41 +0200

    ex 3

--- ProjectManagement/Controllers/HomeController.cs
+++ ProjectManagement/Controllers/HomeController.cs
@@ -29,5 +29,11 @@ namespace ProjectManagement.Controllers
 
             return View();
         }
+
+        public double CalcAverage()
+        {
+            var list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            return list.Average();
+        }
     }
 }

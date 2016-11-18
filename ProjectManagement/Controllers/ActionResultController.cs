ba8049da7b08a8c956826778c123bcc7b8d80f1e
flvdnc <F@CEPUS>
2016-11-18 12:45:21 +0200

    ex 8

---
+++ ProjectManagement/Controllers/ActionResultController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectManagement.Controllers
+{
+    public class ActionResultController : Controller
+    {
+        // GET: ActionResult
+        public ActionResult Index()
+        {
+            return Content("This message is sent from the Index action!");
+        }
+
+        public ActionResult RedirectPermanent()
+        {
+            return RedirectPermanent(@"https://www.google.com");
+        }
+
+        public ActionResult RedirectToActionInTheSameController()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
